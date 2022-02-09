61d6ce7908f2b30f13eec4e50ffa83743b0d5d53
Amir hossein Zare <a.zare137792@yahoo.com>
2022-02-09 13:40:26 +0330

    Creating project. Generating backend. Building frontend. configuring liferay. creating portlet. Writing an administrative portlet. Displaying messages and errors. Using Resources and Permissions. Search and Indexing.

---
+++ modules/guestbook/guestbook-api/src/main/java/com/liferay/docs/guestbook/constants/GuestbookWebPortletKeys.java
@@ -0,0 +1,12 @@
+package com.liferay.docs.guestbook.constants;
+
+/**
+ * @author Amir
+ */
+public class GuestbookWebPortletKeys {
+
+	public static final String GUESTBOOK =
+		"com_liferay_docs_guestbook_portlet_GuestbookWebPortlet";
+	public static final String GUESTBOOK_ADMIN =
+			"com_liferay_docs_guestbook_portlet_GuestbookAdminPortlet";
+}
